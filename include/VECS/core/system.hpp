5c557aa78b614b54503cf596f23c1c5a5ac0a8a2
Sicheng Wang <sicheng.wang@tum.de>
2024-03-11 15:33:08 +0100

    implemented system manager.

--- include/VECS/core/system.hpp
+++ include/VECS/core/system.hpp
@@ -1,28 +1,70 @@
 #pragma once
 #include "common.hpp"
 
+#include <any>
+#include <cassert>
+#include <memory>
 #include <set>
 #include <unordered_map>
 
 struct System
 {
-    std::set<Entity> RuledEnities{};
-    virtual void Update();
+	std::set<Entity> mEntities{};
+	virtual void Update();
 };
 
 class SystemManager
 {
-    std::unordered_map<char const *,Signature> systemSignatures{};
-    std::unordered_map<char const *,System*> systemPointers{};
-public:
-    
-    template<typename T>
-    T* RegisterSystem();
+	std::unordered_map<char const *, Signature> mSignatures{};
+	std::unordered_map<char const *, std::shared_ptr<System>> mSystems{};
 
-    template<typename T>
-    void SetSignature(Signature signature);
-    
-    void RemoveEntity(Entity entity);
-    
-    void SignatureChanged(Entity entity,Signature signature);
+  public:
+	template <typename T>
+	std::shared_ptr<T> RegisterSystem();
+
+	template <typename T>
+	void SetSignature(Signature signature);
+
+	void RemoveEntity(Entity entity);
+
+	void EntitySignatureChanged(Entity entity, Signature signature);
 };
+
+template <typename T>
+std::shared_ptr<T> SystemManager::RegisterSystem()
+{
+	char const *typeName = typeid(T).name();
+	assert(mSystems.find(typeName) == mSystems.end() &&
+		   "system registered twice.");
+	auto system = std::make_shared<T>();
+	mSystems.insert(typeName, system);
+	return system;
+}
+
+template <typename T>
+void SystemManager::SetSignature(Signature signature)
+{
+
+	char const *typeName = typeid(T).name();
+	assert(mSystems.find(typeName) != mSystems.end() &&
+		   "unknown system.");
+	mSignatures[typeName]=signature;
+}
+
+inline void SystemManager::RemoveEntity(Entity entity)
+{
+		for(auto const & pair:mSystems)
+		{
+			pair.second->mEntities.erase(entity);	
+		}
+}
+inline void SystemManager::EntitySignatureChanged(Entity entity,Signature signature)
+{
+		for(auto const & pair:mSignatures)
+		{
+			if((signature & pair.second)==signature)		
+					mSystems[pair.first]->mEntities.insert(entity);
+			else
+					mSystems[pair.first]->mEntities.erase(entity);
+		}
+}
