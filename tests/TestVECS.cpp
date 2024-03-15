be49fbd22344a1aa53421ed69006e9f8c7b705b9
Sicheng Wang <sicheng.wang@tum.de>
2024-03-15 18:31:52 +0100

    add a template of unit test.

---
+++ tests/TestVECS.cpp
@@ -0,0 +1,26 @@
+//Unit Test for VECS
+
+/**
+#include <gtest/gtest.h>
+#include "VECS/core/vecs.hpp"
+
+// Define a test fixture for MyClass
+class MyClassTest : public ::testing::Test {
+protected:
+    MyClass obj;
+};
+
+// Test case to check a specific functionality of MyClass
+TEST_F(MyClassTest, TestFunctionality) {
+    // Test specific functionality of MyClass
+    EXPECT_EQ(obj.someFunction(2), 4); // Example assertion
+}
+
+**/
+
+// Main function for running the tests
+int main(int argc, char** argv) {
+    //::testing::InitGoogleTest(&argc, argv);
+    //return RUN_ALL_TESTS();
+}
+
