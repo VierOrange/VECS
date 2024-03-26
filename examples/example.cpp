b07714e0a964c91c92a3426c5f5203d0a73bfe84
Sicheng Wang <sicheng.wang@tum.de>
2024-03-26 01:08:48 +0100

    add dt to separate motion and frame rate.

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,8 +1,14 @@
 #include "VECS/core/vecs.hpp"
+#include <chrono>
+#include <ratio>
 
-#include "VECS/syss/LoggingSystem.hpp"
-#include "VECS/syss/RenderSystem.hpp"
+#include "utils.hpp"
+#include "VECS/comps/Velocity.hpp"
+#include "VECS/comps/Transform.hpp"
 #include "VECS/syss/InputSystem.hpp"
+#include "VECS/syss/LoggingSystem.hpp"
+#include "VECS/syss/PhysicsSystem.hpp"
+#include "VECS/syss/RenderSystem.hpp"
 
 namespace VECS
 {
@@ -15,8 +21,8 @@ void Init()
 {
 	WorldPtr				 = std::make_shared<internal::World>();
 	internal::Entity::pWorld = WorldPtr;
-	Exit = false;
-	AssetsPath = std::string{"../assets/images/"};
+	Exit					 = false;
+	AssetsPath				 = std::string{"../assets/images/"};
 }
 void Build()
 {
@@ -24,6 +30,8 @@ void Build()
 	// Register all components here
 	World.RegisterComponent<Render>();
 	World.RegisterComponent<Controllable>();
+	World.RegisterComponent<Transform>();
+	World.RegisterComponent<Velocity>();
 
 	// Register all systems here
 	Signature signature;
@@ -38,25 +46,53 @@ void Build()
 	World.RegisterSystem<InputSystem>();
 	World.SetSystemSignature<InputSystem>(signature);
 
-	signature.set(World.GetComponentType<Render>());
-	World.RegisterSystem<LoggingSystem>();
-	World.SetSystemSignature<LoggingSystem>(signature);
+	signature.reset();
+
+	signature.set(World.GetComponentType<Transform>());
+	signature.set(World.GetComponentType<Velocity>());
+	World.RegisterSystem<PhysicsSystem>();
+	World.SetSystemSignature<PhysicsSystem>(signature);
+
+	//	World.RegisterSystem<LoggingSystem>();
+	//	World.SetSystemSignature<LoggingSystem>(signature);
 
 	// Create initial entities here
-	
-	VECS::internal::Entity background = World.Create();
-	background.AddComponent<Render>(Render{0,0});
-	background.AddComponent<Controllable>(Controllable{0,1});
+
+	// VECS::internal::Entity background = World.Create();
+	// background.AddComponent<Render>(Render{0,0});
+	// background.AddComponent<Controllable>(Controllable{0,1});
+	//
+	VECS::internal::Entity player = World.Create();
+	player.AddComponent<Render>(Render{PLAYER, SILVER,40,40});
+	player.AddComponent<Controllable>(Controllable{CPLAYER, true});
+	player.AddComponent<Transform>(Transform{10, 10});
+	player.AddComponent<Velocity>(Velocity{60, 0});
+	//	player.AddComponent<>({});
+	for (int i = 0; i < 3072; ++i)
+	{
+		VECS::internal::Entity tile = World.Create();
+
+		tile.AddComponent<Render>(Render{TILE,genRand(0,4),10,10});
+		tile.AddComponent<Transform>(Transform{
+			10.f * static_cast<int>(i % 64), 10.f * static_cast<int>(i / 64)});
+	}
 }
 void Run()
 {
 
 	internal::World &World = *WorldPtr;
+	auto last			   = std::chrono::high_resolution_clock::now();
+	auto now			   = std::chrono::high_resolution_clock::now();
+	float dt			   = 0;
 	while (!Exit)
-//	for(int i = 0;i<60;++i)
+	//	for(int i = 0;i<60;++i)
 	{
+		now	 = std::chrono::high_resolution_clock::now();
+		dt	 = std::chrono::duration<float, std::ratio<1>>(now - last).count();
+		last = now;
 		World.GetSystem<InputSystem>()->Update();
-		World.GetSystem<LoggingSystem>()->Update();
+		World.GetSystem<PhysicsSystem>()->Update(dt);
+		//		World.GetSystem<LoggingSystem>()->Update(dt);
 		World.GetSystem<RenderSystem>()->Update();
 	}
 }
--- include/VECS/syss/LoggingSystem.hpp
+++ include/VECS/syss/LoggingSystem.hpp
@@ -3,18 +3,29 @@
 
 #include "VECS/comps/Controllable.hpp"
 #include "VECS/comps/Render.hpp"
+#include "VECS/comps/Velocity.hpp"
+#include "VECS/comps/Transform.hpp"
 #include "VECS/core/vecs.hpp"
 
 struct LoggingSystem : VECS::internal::System
 {
-	void Update()
+	float interval;
+	void Update(float dt)
 	{
 		for (VECS::internal::Entity en : mEntities)
 		{
-			Render &render	  = en.GetComponent<Render>();
-			Controllable &con = en.GetComponent<Controllable>();
-			std::printf("Render: %d %d\n",render.type ,render.index );
-			std::printf("Controllable: %d %d\n",con.type ,con.isControlled );
+			interval += dt;
+			if (interval > 1)
+			{
+				interval			 = 0;
+				Transform &transform = en.GetComponent<Transform>();
+			Velocity &velocity = en.GetComponent<Velocity>();
+				std::printf("x: %f\ty: %f\tvx: %d\tvy: %d\n",
+							transform.x,
+							transform.y,
+							velocity.vx,
+							velocity.vy);
+			}
 		}
 	}
 };
