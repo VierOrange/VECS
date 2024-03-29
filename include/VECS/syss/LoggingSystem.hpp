774482d6901b39fee1ae7e0ea564d219ba65e34c
Sicheng Wang <sicheng.wang@tum.de>
2024-03-29 17:10:38 +0100

    log frame rate and entity amount.

--- include/VECS/syss/LoggingSystem.hpp
+++ include/VECS/syss/LoggingSystem.hpp
@@ -3,29 +3,28 @@
 
 #include "VECS/comps/Controllable.hpp"
 #include "VECS/comps/Render.hpp"
-#include "VECS/comps/Velocity.hpp"
 #include "VECS/comps/Transform.hpp"
+#include "VECS/comps/Velocity.hpp"
 #include "VECS/core/vecs.hpp"
 
 struct LoggingSystem : VECS::internal::System
 {
-	float interval;
+	float mInterval;
+	int mFramesCount;
 	void Update(float dt)
 	{
-		for (VECS::internal::Entity en : mEntities)
+		mInterval += dt;
+		++mFramesCount;
+		if (mInterval > 1)
 		{
-			interval += dt;
-			if (interval > 1)
-			{
-				interval			 = 0;
-				Transform &transform = en.GetComponent<Transform>();
-			Velocity &velocity = en.GetComponent<Velocity>();
-				std::printf("x: %f\ty: %f\tvx: %d\tvy: %d\n",
-							transform.x,
-							transform.y,
-							velocity.vx,
-							velocity.vy);
-			}
+			mInterval = 0;
+			std::printf("[FPS]: %d\t[Entity Count]: %d\n",
+						mFramesCount,
+						int(mEntities.size()));
+			mFramesCount = 0;
 		}
+		//for (VECS::internal::Entity en : mEntities)
+		//{
+		//}
 	}
 };
